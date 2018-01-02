f438d3660f8ce20aeb00ceb66e3b680f6b96fcb9
Murthy10 <samuel.kurath@gmail.com>
2018-01-02 19:09:37 +0100

    added obe abe rule

--- pyschieber/stich.py
+++ pyschieber/stich.py
@@ -1,3 +1,23 @@
 from collections import namedtuple
 
-Stich = namedtuple('Stich', ['player', 'cards', 'trumpf'])
+from pyschieber.trumpf import Trumpf
+
+PlayedCard = namedtuple('PlayedCard', ['player', 'card'])
+
+Stich = namedtuple('Stich', ['player', 'played_cards', 'trumpf'])
+
+
+def obe_abe(played_cards):
+    suit = played_cards[0].card.suit
+    value = played_cards[0].card.value
+    stich = Stich(player=played_cards[0].player, played_cards=played_cards, trumpf=Trumpf.OBE_ABE)
+    for i in range(1, len(played_cards)):
+        current_suit = played_cards[i].card.suit
+        current_value = played_cards[i].card.value
+        if suit == current_suit and current_value > value:
+            stich = Stich(player=played_cards[i].player, played_cards=played_cards, trumpf=Trumpf.OBE_ABE)
+            value = current_value
+    return stich
+
+
+stich_rules = {Trumpf.OBE_ABE: obe_abe}
