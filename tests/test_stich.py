b25d0c05f3a22ddbb6e92cc9195761cfcad274ad
Murthy10 <samuel.kurath@gmail.com>
2018-01-03 19:05:43 +0100

    added trumpf rules

--- tests/test_stich.py
+++ tests/test_stich.py
@@ -7,7 +7,9 @@ from pyschieber.trumpf import Trumpf
 @pytest.mark.parametrize("trumpf, index,", [
     (Trumpf.OBE_ABE, 2),
     (Trumpf.UNDE_UFE, 3),
+    (Trumpf.BELL, 3),
+    (Trumpf.ACORN, 1),
 ])
-def test_stich_obe_abe(trumpf, index, players, played_cards):
+def test_stich(trumpf, index, players, played_cards):
     stich = stich_rules[trumpf](played_cards=played_cards)
     assert stich.player is players[index]
