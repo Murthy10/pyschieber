9218a993adfdd9584231fe351361e2092fd0c264
Murthy10 <samuel.kurath@gmail.com>
2018-02-21 13:57:31 +0100

    added tests to example_trumpf

--- example/example_trumpf.py
+++ example/example_trumpf.py
@@ -1,34 +1,45 @@
+from enum import Enum
+
 from pyschieber.suit import Suit
 from pyschieber.trumpf import Trumpf
 
-
 # https://www.jassverzeichnis.ch/index.php/blog/95-jass-tipps-trumpfansagen-schieber
+TrumpfType = Enum('TrumpfType',
+                  ['UNDER_4', 'NELL_ASS_5', 'UNDER_NELL_ASS', 'UNDER_NELL_3_2_ASS', 'STICHE_5', 'NO_TRUMPF'])
+
+
 def choose_trumpf(cards, geschoben):
     candidates = []
     for trumpf in filter(lambda x: x != Trumpf.OBE_ABE and x != Trumpf.UNDE_UFE and x != Trumpf.SCHIEBEN, Trumpf):
-        if evalute_stich_trumpf(cards, trumpf):
-            candidates.append(trumpf)
-    if evalute_obe_abe(cards):
-        candidates.append(Trumpf.OBE_ABE)
-    if evalute_obe_abe(cards):
-        candidates.append(Trumpf.UNDE_UFE)
+        trumpf_type = evalute_stich_trumpf(cards, trumpf.name)
+        if not trumpf_type == TrumpfType.NO_TRUMPF:
+            candidates.append((trumpf, trumpf_type))
+    trumpf_type = evalute_unde_ufe(cards)
+    if not trumpf_type == TrumpfType.NO_TRUMPF:
+        candidates.append((Trumpf.UNDE_UFE, trumpf_type))
+    trumpf_type = evalute_obe_abe(cards)
+    if not trumpf_type == TrumpfType.NO_TRUMPF:
+        candidates.append((Trumpf.OBE_ABE, trumpf_type))
     if len(candidates) >= 1:
         return candidates[0]
-    return Trumpf.SCHIEBEN
+    return Trumpf.SCHIEBEN, TrumpfType.NO_TRUMPF
 
 
-def evalute_stich_trumpf(cards, trumpf):
-    trumpf_card_values = [card.value for card in cards if card.suit.name == trumpf.name]
-    non_trumpf_card_values = [card for card in cards if card.suit.name != trumpf.name]
+def evalute_stich_trumpf(cards, suit_name):
+    trumpf_card_values = [card.value for card in cards if card.suit.name == suit_name]
+    non_trumpf_card_values = [card.value for card in cards if card.suit.name != suit_name]
     nell = bool(9 in trumpf_card_values)
-    under = bool(12 in trumpf_card_values)
+    under = bool(11 in trumpf_card_values)
     ass = bool(14 in trumpf_card_values)
     if under and len(trumpf_card_values) == 4:
-        return True
+        return TrumpfType.UNDER_4
     if ass and nell and len(trumpf_card_values) == 5:
-        return True
-    if ass and nell and under and non_trumpf_card_values.count(14) >= 2:
-        return True
+        return TrumpfType.NELL_ASS_5
+    if ass and nell and under:
+        return TrumpfType.UNDER_NELL_ASS
+    if nell and under and non_trumpf_card_values.count(14) >= 2:
+        return TrumpfType.UNDER_NELL_3_2_ASS
+    return TrumpfType.NO_TRUMPF
 
 
 def evalute_unde_ufe(cards):
@@ -47,7 +58,7 @@ def evalute_unde_ufe(cards):
         if best_card >= 8:
             for card in suit_card_copy:
                 stiche.append((suit, card))
-    return len(stiche) > 5
+    return TrumpfType.STICHE_5 if len(stiche) > 5 else TrumpfType.NO_TRUMPF
 
 
 def evalute_obe_abe(cards):
@@ -63,10 +74,10 @@ def evalute_obe_abe(cards):
                 best_card -= 1
             else:
                 break
-        if best_card >= 12:
+        if best_card <= 12:
             for card in suit_card_copy:
                 stiche.append((suit, card))
-    return len(stiche) > 5
+    return TrumpfType.STICHE_5 if len(stiche) > 5 else TrumpfType.NO_TRUMPF
 
 
 def split_cards_by_suit(cards):
---
+++ tests/example/test_example_trumpf.py
@@ -0,0 +1,31 @@
+import pytest
+
+from example.example_trumpf import choose_trumpf, TrumpfType
+from pyschieber.card import Card
+from pyschieber.suit import Suit
+from pyschieber.trumpf import Trumpf
+
+
+@pytest.mark.parametrize("cards, trumpf, trumpf_type", [
+    ([Card(Suit.BELL, 13), Card(Suit.BELL, 12), Card(Suit.BELL, 11), Card(Suit.BELL, 8), Card(Suit.ACORN, 12),
+      Card(Suit.ACORN, 11), Card(Suit.SHIELD, 12), Card(Suit.SHIELD, 7), Card(Suit.ROSE, 9)], Trumpf.BELL,
+     TrumpfType.UNDER_4),
+    ([Card(Suit.BELL, 14), Card(Suit.BELL, 12), Card(Suit.BELL, 9), Card(Suit.BELL, 8), Card(Suit.BELL, 6),
+      Card(Suit.ACORN, 11), Card(Suit.SHIELD, 12), Card(Suit.SHIELD, 7), Card(Suit.ROSE, 9)], Trumpf.BELL,
+     TrumpfType.NELL_ASS_5),
+    ([Card(Suit.BELL, 14), Card(Suit.BELL, 11), Card(Suit.BELL, 9), Card(Suit.ACORN, 12), Card(Suit.ACORN, 12),
+      Card(Suit.ACORN, 7), Card(Suit.SHIELD, 12), Card(Suit.SHIELD, 7), Card(Suit.ROSE, 9)], Trumpf.BELL,
+     TrumpfType.UNDER_NELL_ASS),
+    ([Card(Suit.BELL, 11), Card(Suit.BELL, 9), Card(Suit.BELL, 6), Card(Suit.ACORN, 14), Card(Suit.ACORN, 12),
+      Card(Suit.ACORN, 7), Card(Suit.SHIELD, 14), Card(Suit.SHIELD, 7), Card(Suit.ROSE, 9)], Trumpf.BELL,
+     TrumpfType.UNDER_NELL_3_2_ASS),
+    ([Card(Suit.BELL, 14), Card(Suit.BELL, 13), Card(Suit.BELL, 12), Card(Suit.BELL, 10), Card(Suit.BELL, 6),
+      Card(Suit.ACORN, 7), Card(Suit.SHIELD, 14), Card(Suit.ROSE, 13), Card(Suit.ROSE, 12)], Trumpf.OBE_ABE,
+     TrumpfType.STICHE_5),
+    ([Card(Suit.BELL, 6), Card(Suit.BELL, 7), Card(Suit.BELL, 8), Card(Suit.BELL, 10), Card(Suit.ACORN, 6),
+      Card(Suit.ACORN, 7), Card(Suit.SHIELD, 14), Card(Suit.ROSE, 13), Card(Suit.ROSE, 12)], Trumpf.UNDE_UFE,
+     TrumpfType.STICHE_5),
+])
+def test_choose_trumpf_no_schieben(cards, trumpf, trumpf_type):
+    evaluate_trumpf, evaluate_trumpf_type = choose_trumpf(cards=cards, geschoben=False)
+    assert (evaluate_trumpf, evaluate_trumpf_type) == (trumpf, trumpf_type)
