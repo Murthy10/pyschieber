1755dc57116900b8fe3cee4fad2ba959879ec053
Murthy10 <samuel.kurath@gmail.com>
2018-02-21 14:08:21 +0100

    fixed base_player

--- tests/player/test_base_player.py
+++ tests/player/test_base_player.py
@@ -1,7 +1,9 @@
 from pyschieber.player.base_player import BasePlayer
+from itertools import count
 
 
 def test_base_player_counter():
+    BasePlayer.class_counter = count(0)
     base_player = [BasePlayer(name=i) for i in range(4)]
 
     for i, player in enumerate(base_player):
