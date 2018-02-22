91d1364bbfd08571f17f78df86175811f9d2e1ca
Murthy10 <samuel.kurath@gmail.com>
2018-02-22 18:04:57 +0100

    stated with network_player

---
+++ pyschieber/player/network_player.py
@@ -0,0 +1,54 @@
+import logging
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+from pyschieber.player.base_player import BasePlayer
+
+
+class NetworkPlayer(BasePlayer):
+    @classmethod
+    def with_port(cls, name, port=3000):
+        player = cls(name=name)
+        player.port = port
+        return cls
+
+    def choose_trumpf(self, geschoben):
+        pass
+
+    def choose_card(self):
+        pass
+
+    def stich_over(self):
+        pass
+
+    def run(self, server_class=HTTPServer, handler_class=RequestHandler, port=3000):
+        logging.basicConfig(level=logging.INFO)
+        server_address = ('', port)
+        httpd = server_class(server_address, handler_class)
+        logging.info('Starting httpd...\n')
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            pass
+        httpd.server_close()
+        logging.info('Stopping httpd...\n')
+
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        self._set_response()
+        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
+        post_data = self.rfile.read(content_length)  # <--- Gets the data itself
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                     str(self.path), str(self.headers), post_data.decode('utf-8'))
+
+        self._set_response()
+        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
