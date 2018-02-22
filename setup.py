a64bd572f8313c01ef786f7a6e3e9adc594844e7
Murthy10 <samuel.kurath@gmail.com>
2018-02-22 10:03:14 +0100

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
     setup(
         name='pyschieber',
-        version='0.1.0',
+        version='0.1.1',
         description='pyschieber is a terminal application of the popular swiss card game Schieber and provides an API to the game',
         long_description=readme,
         author='Samuel Kurath',
