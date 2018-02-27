542c8c728a269a7996d20eabbb131a9d01d1ac91
Murthy10 <samuel.kurath@gmail.com>
2018-02-27 18:51:23 +0100

    added statistical test

--- pyschieber/tournament.py
+++ pyschieber/tournament.py
@@ -1,7 +1,6 @@
 import logging
 
 from pyschieber.game import Game
-from pyschieber.player.base_player import BasePlayer
 from pyschieber.team import Team
 
 logger = logging.getLogger(__name__)
@@ -28,7 +27,7 @@ class Tournament:
         self.check_players()
         team_1 = Team(players=[self.players[0], self.players[2]])
         team_2 = Team(players=[self.players[1], self.players[2]])
-        self.teams += [team_1, team_2]
+        self.teams = [team_1, team_2]
 
     def play(self):
         self.build_teams()
@@ -40,9 +39,11 @@ class Tournament:
             logger.info('-' * 200)
             logger.info('Round {} starts.'.format(len(self.games)))
             logger.info('-' * 200)
-            end = game.play()
+            end = game.play(start_player_index=((len(self.games) - 1) % 4))
             logger.info('Round {} is over.'.format(len(self.games)))
             logger.info('Points: Team 1: {0} , Team 2: {1}. \n'.format(self.teams[0].points, self.teams[1].points))
+        winning_team = 0 if self.teams[0].won(point_limit=self.point_limit) else 1
+        logger.info('Team {0} won! \n'.format(winning_team))
 
     def get_status(self):
         return {
---
+++ tests/player/test_random_player.py
@@ -0,0 +1,33 @@
+import pytest
+from math import sqrt, floor
+
+from pyschieber.player.random_player import RandomPlayer
+from pyschieber.tournament import Tournament
+
+
+@pytest.mark.statistical
+def test_is_random():
+    point_limit = 1000
+    number_of_tournaments = 200
+    mean = number_of_tournaments * 0.5  # assume that a RandomPlayer has a 50% chance to win
+    variance = mean * (1 - 0.5)
+    standard_deviation = floor(sqrt(variance))
+
+    random_players = [RandomPlayer(name=i) for i in range(4)]
+    tournament = Tournament(point_limit=point_limit)
+    [tournament.register_player(player=player) for player in random_players]
+
+    team_1_won = 0
+    team_2_won = 0
+    for _ in range(number_of_tournaments):
+        tournament.play()
+        if tournament.teams[0].won(point_limit=point_limit):
+            team_1_won += 1
+        else:
+            team_2_won += 1
+
+    difference = abs(team_1_won - team_2_won)
+    print("Difference: ", difference)
+    print("Team 1: ", team_1_won)
+    print("Team 2: ", team_2_won)
+    assert difference in range(0, 2 * standard_deviation)
