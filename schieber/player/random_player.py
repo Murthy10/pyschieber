4710bb15c4d48608239d4f02955eb3b860368d66
Joel Niklaus <me@joelniklaus.ch>
2018-12-02 12:07:09 +0100

    added small bugfix

--- schieber/player/random_player.py
+++ schieber/player/random_player.py
@@ -6,8 +6,7 @@ from schieber.trumpf import Trumpf
 
 class RandomPlayer(BasePlayer):
     def choose_trumpf(self, geschoben):
-        return Trumpf.OBE_ABE  # always choose obe abe for now
-
+        return self.move(choices=[Trumpf.OBE_ABE])  # always choose obe abe for now
         # return self.move(choices=list(Trumpf))
 
     def choose_card(self, state=None):
